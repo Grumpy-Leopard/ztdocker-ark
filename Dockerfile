628b40e02bad071b1ddd4a170738f8c21a30be43
Jye Karl-Perry <me@jye.guru>
2020-01-26 20:43:40 +1100

    Update all folders to be inside user home due to permission issues

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM cm2network/steamcmd
 WORKDIR /home/steam
 USER steam
 
-RUN mkdir -p /ark/ShooterGame/Saved/SavedArks
+RUN mkdir -p /home/steam/ark/ShooterGame/Saved/SavedArks
 COPY update_and_start.sh /home/steam/update_and_start.sh
 
 ENV SESSIONNAME="ARK Server in Docker" \
@@ -13,7 +13,7 @@ ENV SESSIONNAME="ARK Server in Docker" \
     SERVERADMINPASSWORD="YouShouldChangeThisWhenDeploying" \
     MAXPLAYERS="50"
 
-VOLUME /ark
-VOLUME /ark/ShooterGame/Saved/SavedArks
+VOLUME /home/steam/ark
+VOLUME /home/steam/ark/ShooterGame/Saved/SavedArks
 
 CMD ["./update_and_start.sh"]
--- update_and_start.sh
+++ update_and_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./steamcmd.sh +login anonymous +force_install_dir /ark +app_update 376030 validate +quit
-/ark/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?Multihome=0.0.0.0?SessionName=$SESSIONNAME?MaxPlayers=$MAXPLAYERS?QueryPort=$QUERYPORT?RCONPort=$RCONPORT?Port=$PORT?ServerAdminPassword=$SERVERADMINPASSWORD -server -log
+./ark/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?Multihome=0.0.0.0?SessionName=$SESSIONNAME?MaxPlayers=$MAXPLAYERS?QueryPort=$QUERYPORT?RCONPort=$RCONPORT?Port=$PORT?ServerAdminPassword=$SERVERADMINPASSWORD -server -log
